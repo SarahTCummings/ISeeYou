edd6491a7354ec6fa8391e9d784db5b5d6f4ab0c
SarahTCummings <stc@umumvid.io>
2017-11-28 16:48:02 -0500

    CSS

--- src/components/Email.js
+++ src/components/Email.js
@@ -13,8 +13,8 @@ class Email extends Component {
     return (
       <div className="Email" onSubmit={this.handleSubmit.bind(this)}>
         <form className="email_form">
-          <label htmlFor="email_form"> What is your email address? </label>
-          <input id="email_form" type="text" placeholder="Email Address" value={this.state.email} onChange={this.handleEmailChange.bind(this)}/>
+          <label htmlFor="email_form"> <h1> Enter your email address. </h1> </label>
+          <input id="email_form" type="text" placeholder="you@gmail.com" value={this.state.email} onChange={this.handleEmailChange.bind(this)}/>
           <button type="submit">Submit</button>
         </form>
       </div>
