cda1ef0be4d02d8ab16987f8c714e65a59a3118f
Chemley <canaowens@gmail.com>
2017-11-29 12:03:16 -0500

    added some CSS for the picture pages

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import Pongo from '../images/pongo.jpg';
+import Pongo from '../images/pongo.jpg';
 
 class Picture extends Component {
   render() {
     return (
       <div className="Picture">
-        <h1>Hello {this.props.name} &#128065;</h1>
-        <img src={this.props.data.photos[1].url} alt="David"/>
+        <img className="Giant_eye" src={Pongo} alt="giant eye"/>
+        <img className="Giant_eye_cape" src={Pongo} alt="giant eye with cape"/>
+        <h1>&#128065; Hello {this.props.name} &#128065;</h1>
+        <img className="Picture_initial" src={this.props.data.photos[1].url} alt="David"/>
         <button type="submit">Show Me More</button>
       </div>
     );
