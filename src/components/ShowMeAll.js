f750c66d3f1fcfb51deddaaa1050e90e1f3cf827
tkat7 <tkatona2013@gmail.com>
2017-11-29 13:17:01 -0500

    show me contact info

--- src/components/ShowMeAll.js
+++ src/components/ShowMeAll.js
@@ -7,40 +7,50 @@ class ShowMeAll extends Component {
       <div className="ShowMeAll">
         <p>show</p>
         <div className="Summary">
-          <img className="Photo0"/>
-          <div className="ShowMeName"></div>
-          <div className="ShowMeDemographics"></div>
-          <div className="ShowMeOrganizations"></div>
+          <img className="Photo0" src={this.props.photos ? this.props.photos[0].url : null}/>
+          <div className="ShowMeName">{this.props.contactInfo ? <p>{this.props.contactInfo.fullName}</p> : null}</div>
+          <div className="ShowMeDemographics">{this.props.demographics ? [
+              <p>{this.props.demographics.gender || null}</p>,
+              <p>{this.props.demographics.age || null}</p>,
+              <p>{this.props.demographics.locationGeneral || null}</p>] : null}
+            </div>
+          <div className="ShowMeOrganizations">{this.props.organizations ? [
+              <p>{this.props.organizations.title || null}</p>,
+              <p>{this.props.organizations.name || null}</p>,
+              <p>{this.props.organizations.startDate || null}</p>,
+              <p>{this.props.organizations.endDate || null}</p>,
+              <p>{this.props.organizations.current || null}</p>] : null}
+        </div>
         </div>
 
           {
           this.props.socialProfiles ?
           this.props.socialProfiles.map((profile) => (
-            <div className={profile.typeId}>
+            profile.type !== 'klout' ?
+            <div key={profile.typeId}>
               <p>{profile.typeName || ""}</p>
               <p>{profile.bio || ""}</p>
               <p>{profile.followers ? "Followers: " + profile.followers : ""}</p>
               <p>{profile.following || ""}</p>
-            </div>
+            </div> : null
           ))
           : null
         }
-        {/* {
-        this.props.contactInfo ?
-        this.props.contactInfo.map((contact) => (
-          <div className={contact.typeId}>
-            <p>{profile.typeName || ""}</p>
-            <p>{profile.bio || ""}</p>
-            <p>{profile.followers ? "Followers: " + profile.followers : ""}</p>
-            <p>{profile.following || ""}</p>
+        {
+          this.props.contactInfo ?
+          <div className="contactInfo">
+            <p>{this.props.contactInfo.givenName || ""}</p>
+          {this.props.contactInfo.middleNames ? this.props.contactInfo.middleNames.map((name) => (
+            <p key={name}>{name}</p>
+          )) : null}
+            <p>{this.props.contactInfo.familyName || ""}</p>
           </div>
-        ))
-        : null
-        } */}
+          : null
+        }
         {
         this.props.photos ?
         this.props.photos.map((photo) => (
-        <div className={photo.typeId}>
+        <div key={photo.typeId}>
           <img src={photo.url}/>
         </div>
         ))
@@ -51,12 +61,17 @@ class ShowMeAll extends Component {
     );
   }
 }
+// TODO: ORGS, KLOUT, websites, KEYS
 
 function mapStateToProps(state) {
+  console.log(state.data.contactInfo);
   return {
     data: state.data,
     socialProfiles: state.data.socialProfiles,
-    photos: state.data.photos
+    photos: state.data.photos,
+    contactInfo: state.data.contactInfo,
+    demographics: state.data.demographics,
+    organizations: state.data.organizations
   };
 }
 
