5edb189bc0591a71b0bf9c509aebc408376425df
Chemley <canaowens@gmail.com>
2017-11-30 17:20:24 -0500

    got api working and additional changes made

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 // Server.js. Must have the top two lines, always.
 var express = require("express");
 var app = express();
-
+var request = require("request");
 
 app.get("/getdata", function(req, res) {
   console.log(req.query.email);
   var email = req.query.email;
+  var options = {
+    url: "https://api.fullcontact.com/v2/person.json?email=" + email,
+    headers: {
+      "X-FullContact-APIKey": "aa1febea3072edd9",
+      json: true
+    }
+  }
 
-
- // url: "https://api.fullcontact.com/v2/person.json?email=" + email
- // headers: {"X-FullContact-APIKey": "aa1febea3072edd9"}
  request(options, function(error, response, body) {
    console.log(body);
-   res.send("hi");
-   options : {
-     url: "https://api.fullcontact.com/v2/person.json?email=" + email
-     headers: {"X-FullContact-APIKey": "aa1febea3072edd9"}
-   }
- })
-})
+   res.send(JSON.parse(body));
+ });
+});
 
 
 var server = app.listen(5000, function () {
