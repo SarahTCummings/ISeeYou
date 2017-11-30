c2bea031a9fbb5868bf1dbc6596176ebd1491237
Chemley <canaowens@gmail.com>
2017-11-30 10:50:00 -0500

    added a roll down animation

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -10,7 +10,7 @@ class Picture extends Component {
         <img className="Giant_eye" src={Pongo} alt="giant eye"/>
         <img className="Giant_eye_cape" src={Pongo} alt="giant eye with cape"/>
         <h1>&#128065; Hello {this.props.name} &#128065;</h1>
-        <img className="Picture_initial" src={this.props.data.photos[1].url} alt="David"/>
+        {/* <img className="Picture_initial" src={this.props.data.photos[1].url} alt="David"/> */}
         <button onClick={this.props.next} type="submit">Show Me More</button>
       </div>
     );
