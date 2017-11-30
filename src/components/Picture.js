17d95f794ad043b4ab67d657d89eba44ad99b5ff
SarahTCummings <stc@umumvid.io>
2017-11-30 15:53:15 -0500

    css

---
+++ src/components/Picture.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Pongo from '../images/pongo.jpg';
+import { next } from '../actions';
+
+class Picture extends Component {
+  render() {
+    return (
+      <div className="Picture">
+        <img className="Giant_eye" src={Pongo} alt="giant eye"/>
+        <img className="Giant_eye_cape" src={Pongo} alt="giant eye with cape"/>
+        <div className="Picture_initial_container">
+          <h1 className="Picture_initial_header">&#128065; Hello {this.props.name} &#128065;</h1>
+          <img className="Picture_initial" src={this.props.data.photos[1].url} alt="David"/>
+          <button className="Picture_initial_button" onClick={this.props.next} type="submit">Show Me More</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    data: state.data,
+    name: state.data.contactInfo.fullName
+  };
+}
+
+const mapActionsToProps = {
+  next
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(Picture);
