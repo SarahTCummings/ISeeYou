9d63c26d7a7d7c2e15ea81c13e941346a9fb648a
SarahTCummings <stc@umumvid.io>
2017-12-04 16:08:31 -0500

    too many things should have committed earlier

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { next } from '../actions';
 import { connect } from 'react-redux';
 import Eye2 from '../images/eye2.png';
+import Pupil from '../images/pupil.png';
 import Cloaknoeyesmll from '../images/cloaknoeyesmll.png';
 import ShadowFigure from '../images/ShadowFigure.png';
 import Eyeball from '../images/eyeball.png';
@@ -16,8 +17,9 @@ class WelcomePage extends Component {
             <img className="WelcomeFigure2" src={ShadowFigure} alt="shadowy figure"></img>
           </div>
             <img className="WelcomeMobile" src={Cloaknoeyesmll} alt="shadowy figure"></img>
-            <img className="WelcomeMobileEye " src={Eye2} alt="graphic eye"></img>
             <img className="WelcomMoblileClosedEye" src={Eyeball} alt="closed eye"></img>
+            <img className="WelcomeMobileEye " src={Eye2} alt="graphic eye"></img>
+            <img className="WelcomeMobilePupil " src={Pupil} alt="pupil"></img>
           <div className="WelcomeBox">
             <h1> Welcome, human. <br></br> Would you like to know what I see? </h1>
             <button onClick={this.props.next} type="submit">Yes, continue</button>
