9c18a324b2f0cc5cc811b79f9c1f3ae64ffe1c8e
Chemley <canaowens@gmail.com>
2017-12-04 09:52:01 -0500

    cleaned up a few errors in the welcome page and the reducers index.js

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -16,17 +16,14 @@ class WelcomePage extends Component {
             <img className="WelcomeFigure2" src={ShadowFigure} alt="shadowy figure"></img>
 <<<<<<< HEAD
           </div> */}
-=======
-          </div>
-            <img className="WelcomeMobile" src={Cloaknoeyesmll} alt="shadowy figure"></img>
-            <img className="WelcomeMobileEye " src={Eye2} alt="graphic eye"></img>
-            <img className="WelcomMoblileClosedEye" src={Eyeball} alt="closed eye"></img>
->>>>>>> b756ce6e4bacb566da2f15cdf600d4c1554f5bea
+            <img className="WelcomeMobile" src={Cloaknoeyesmll} alt="shadowy figure"/>
+            <img className="WelcomeMobileEye " src={Eye2} alt="graphic eye"/>
+            <img className="WelcomMoblileClosedEye" src={Eyeball} alt="closed eye" />
           <div className="WelcomeBox">
             <h1> Welcome, human. <br></br> Would you like to know what I see? </h1>
             <button onClick={this.props.next} type="submit">Yes, continue</button>
           </div>
-  </div>
+        </div>
     );
   }
 }
