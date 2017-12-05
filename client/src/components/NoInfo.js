2e00f08164fd4a9f432345ded67b21a01288e4fa
tkat7 <tkatona2013@gmail.com>
2017-12-05 11:06:07 -0500

    noinfo

---
+++ client/src/components/NoInfo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import FigureWithEye from '../images/figurewitheye.png';
+import { next } from '../actions';
+
+class NoInfo extends Component {
+  render() {
+    return (
+      <div className="NoInfo">
+          <h1 className="NoInfo_header">&#128065; Well done, You have hidden yourself from me &#128065;</h1>
+      </div>
+    );
+  }
+}
+
+export default connect(NoInfo);
