fe2b9807482023980b533dfb8382ebb2f68b2ad9
Chemley <canaowens@gmail.com>
2017-12-07 14:42:44 -0500

    moved words over

--- client/src/components/NoInfo.js
+++ client/src/components/NoInfo.js
@@ -3,13 +3,32 @@ import { connect } from 'react-redux';
 import FigureWithEye from '../images/figurewitheye.png';
 import { back } from '../actions';
 
+
 class NoInfo extends Component {
   render() {
     return (
       <div className="NoInfo">
-          <h1 className="NoInfo_header">Well done! You have hidden yourself from me.</h1>
-          <button className="NoInfo_button" onClick={this.props.back} type="submit">Enter a Different Email</button>
-      </div>
+        <div className="NoInfo_Words">
+          <h1 className="NoInfo_header">Well done!</h1><h1>You have hidden yourself from me</h1>
+          <button className="NoInfo_button"onClick={this.props.back} type="submit">Enter a Different Email</button>
+        </div>
+          <div class="newcharacter404">
+            <div class="chair404"></div>
+            <div class="leftshoe404"></div>
+            <div class="rightshoe404"></div>
+            <div class="legs404"></div>
+            <div class="torso404">
+              <div class="body404"></div>
+              <div class="leftarm404"></div>
+              <div class="rightarm404"></div>
+              <div class="head404">
+              </div>
+            </div>
+            <div class="laptop404">
+            </div>
+          </div>
+    </div>
+
     );
   }
 }
