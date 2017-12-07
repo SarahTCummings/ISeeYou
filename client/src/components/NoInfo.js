7a75045e056f8de9c6f2f59aa0be05688fd55a96
SarahTCummings <stc@umumvid.io>
2017-12-07 14:39:39 -0500

    animation

--- client/src/components/NoInfo.js
+++ client/src/components/NoInfo.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import FigureWithEye from '../images/figurewitheye.png';
 import { back } from '../actions';
+import Eyelid404 from '../images/eyelid404.png';
+import Pupil8 from '../images/404pupil.png';
 
 
 class NoInfo extends Component {
@@ -19,8 +21,7 @@ class NoInfo extends Component {
               <div class="body404"></div>
               <div class="leftarm404"></div>
               <div class="rightarm404"></div>
-              <div class="head404">
-              </div>
+              <div class="head404"></div>
             </div>
             <div class="laptop404"></div>
   </div>
