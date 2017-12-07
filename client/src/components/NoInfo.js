98d35e9401a2656a4b0bb93c2b5775bd6d7e8521
SarahTCummings <stc@umumvid.io>
2017-12-07 13:59:59 -0500

    animation

--- client/src/components/NoInfo.js
+++ client/src/components/NoInfo.js
@@ -3,13 +3,30 @@ import { connect } from 'react-redux';
 import FigureWithEye from '../images/figurewitheye.png';
 import { back } from '../actions';
 
+
 class NoInfo extends Component {
   render() {
     return (
       <div className="NoInfo">
           <h1 className="NoInfo_header">&#128065; Well done! You have hidden yourself from me &#128065;</h1>
           <button onClick={this.props.back} type="submit">Enter a Different Email</button>
-      </div>
+          <div class="newcharacter404">
+            <div class="chair404"></div>
+            <div class="leftshoe404"></div>
+            <div class="rightshoe404"></div>
+            <div class="legs404"></div>
+            <div class="torso404">
+              <div class="body404"></div>
+              <div class="leftarm404"></div>
+              <div class="rightarm404"></div>
+              <div class="head404">
+              </div>
+            </div>
+            <div class="laptop404"></div>
+  </div>
+</div>
+
+
     );
   }
 }
